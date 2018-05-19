7caec8537548959d4e66b0d3c8ea4c6b5c8dbf78
icyJoseph <sephxd1234@gmail.com>
2018-05-19 11:25:47 +0200

    feat: async routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,18 +2,30 @@ import React, { Fragment } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-import Home from "./containers/Home";
-import Feed from "./containers/Feed";
+import Loadable from "react-loadable";
 import TopBar from "./components/TopBar";
 import BottomBar from "./components/BottomBar";
+import Loader from "./components/Loader";
+
+const asyncHome = Loadable({
+  loader: () => import("./containers/Home"),
+  loading: Loader,
+  delay: 600
+});
+
+const asyncFeed = Loadable({
+  loader: () => import("./containers/Feed"),
+  loading: Loader,
+  delay: 600
+});
 
 export const Routes = () => (
   <MuiThemeProvider>
     <BrowserRouter>
       <Fragment>
         <TopBar />
-        <Route path="/" exact component={Home} />
-        <Route path="/" exact component={Feed} />
+        <Route path="/" exact component={asyncHome} />
+        <Route path="/" exact component={asyncFeed} />
         <BottomBar />
       </Fragment>
     </BrowserRouter>
