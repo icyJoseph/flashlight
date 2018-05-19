a26c1f4ce4e4f2b1f52ac6abe7bdc8f909b94a68
icyJoseph <sephxd1234@gmail.com>
2018-05-19 15:59:18 +0200

    feat: history view

---
+++ client/src/containers/History.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from "react";
+import { List } from "material-ui/List";
+import Divider from "material-ui/Divider";
+import Subheader from "material-ui/Subheader";
+
+import HistoryCard from "../components/HistoryCard";
+import { previously, HistoryData } from "../constants";
+
+export const Feed = ({ history }) => {
+  const navigateTo = id => () => {
+    return history.push(`/SearchDetail/${id}`);
+  };
+
+  const { entries } = HistoryData;
+
+  return (
+    <div
+      style={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        padding: "1%",
+        marginBottom: "100px"
+      }}
+    >
+      <List>
+        <Subheader>{previously}</Subheader>
+        {entries.map(e => (
+          <Fragment key={e.id}>
+            <HistoryCard {...e} navigateTo={navigateTo(e.id)} />
+            <Divider />
+          </Fragment>
+        ))}
+      </List>
+    </div>
+  );
+};
+export default Feed;
