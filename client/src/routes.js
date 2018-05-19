32a2bb59264f7ce149f77c8166a66c10848f5433
icyJoseph <sephxd1234@gmail.com>
2018-05-19 10:23:53 +0200

    refactor: use Home container

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,17 +1,18 @@
 import React, { Fragment } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import App from "./App";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+import Home from "./containers/Home";
 import Feed from "./containers/Feed";
 import TopBar from "./components/TopBar";
 import BottomBar from "./components/BottomBar";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 export const Routes = () => (
   <MuiThemeProvider>
     <BrowserRouter>
       <Fragment>
         <TopBar />
-        <Route path="/" exact component={App} />
+        <Route path="/" exact component={Home} />
         <Route path="/" exact component={Feed} />
         <BottomBar />
       </Fragment>
