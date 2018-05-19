ef66b508a7e2aeee7ff2c6df3987e785bd8d2121
Fredrik Rahn <lillrahn@live.se>
2018-05-19 09:52:01 +0200

    Removed duplicate use for api

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import api from './api';
 
-import { saveUsers, restoreUsers } from "./user-repository";
-import { saveSearches, restoreSearches } from "./search-repository";
-import { saveEvents, restoreEvents } from "./event-repository";
+import { saveUsers, restoreUsers } from './user-repository';
+import { saveSearches, restoreSearches } from './search-repository';
+import { saveEvents, restoreEvents } from './event-repository';
 
 process.stdin.resume();
 
@@ -14,32 +14,33 @@ const app = express();
 
 app.use('/api', api);
 
-<<<<<<< HEAD
-app.get("/api", (req, res) => {
-  res.send("API response");
-});
-
-app.listen(3333, () => console.log("Started server"));
+app.listen(3333, () => console.log('Started server'));
 
 function restoreData() {
-  Promise.all([restoreUsers(), restoreEvents(), restoreSearches()])
-    .then(() => console.log("Restored data"))
-    .catch((err) => console.error(`Could not restore data: ${err}`));
+	Promise.all([restoreUsers(), restoreEvents(), restoreSearches()])
+		.then(() => console.log('Restored data'))
+		.catch(err => console.error(`Could not restore data: ${err}`));
 }
 
 function saveDataOnExit() {
-  process.on("SIGINT", () => { saveAll(); process.exit(0); });
-  process.on("exit", () => { saveAll(); process.exit(0) });
-  process.on("uncaughtException", (e) => { console.error(e); saveAll(); } );
+	process.on('SIGINT', () => {
+		saveAll();
+		process.exit(0);
+	});
+	process.on('exit', () => {
+		saveAll();
+		process.exit(0);
+	});
+	process.on('uncaughtException', e => {
+		console.error(e);
+		saveAll();
+	});
 }
 
 function saveAll() {
-  console.log('Saving!');
+	console.log('Saving!');
 
-  saveUsers();
-  saveSearches();
-  saveEvents();
+	saveUsers();
+	saveSearches();
+	saveEvents();
 }
-=======
-app.listen(3333, () => console.log('Started server'));
->>>>>>> api
