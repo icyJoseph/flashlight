d56cd0128a174e389ea19719014f99012d19a626
icyJoseph <sephxd1234@gmail.com>
2018-05-19 11:51:05 +0200

    feat: Top bar Navigates to home, history navigates to history

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,16 +1,45 @@
 import React, { Fragment } from "react";
 import AppBar from "material-ui/AppBar";
+import FontIcon from "material-ui/FontIcon";
+import { withRouter } from "react-router-dom";
 
-export const TopBar = () => {
+const HomeIcon = (
+  <FontIcon className="material-icons" style={{ color: "#fff" }}>
+    home
+  </FontIcon>
+);
+
+export const TopBar = ({ history }) => {
+  const navigateToHome = () => {
+    return history.push("/");
+  };
+
   return (
     <Fragment>
       <AppBar
         title="Flashlight"
-        iconClassNameRight="muidocs-icon-navigation-expand-more"
+        onTitleClick={navigateToHome}
+        onLeftIconButtonClick={navigateToHome}
+        iconElementLeft={HomeIcon}
+        iconStyleLeft={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 0
+        }}
+        titleStyle={{
+          flex: 9,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "center"
+        }}
         style={{ backgroundColor: "dodgerblue" }}
       />
     </Fragment>
   );
 };
 
-export default TopBar;
+export default withRouter(TopBar);
