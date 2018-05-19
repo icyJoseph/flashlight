0b90d7f1961237c61a2dabcb798295953fca9ace
icyJoseph <sephxd1234@gmail.com>
2018-05-19 10:23:04 +0200

    chore: add mock data generator

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,1 +1,22 @@
+import jedi from "./img/jedi.png";
+import { darkBlack } from "material-ui/styles/colors";
+
 export const paths = ["history", "flashlight", "add", "me"];
+
+export const info = {
+  title: "Brunch this weekend?",
+  person: { name: "Brendan", lastname: "Lim" },
+  description: `I'll be in your neighborhood doing errands this weekend. Do you
+    want to grab brunch?`,
+  image: jedi,
+  color: darkBlack
+};
+
+export const events = info =>
+  Array.from(
+    new Array(20),
+    (x, i) =>
+      i % 2 === 0
+        ? { ...info, left: true, id: `${i}-info.title` }
+        : { ...info, id: `${i}-info.title`, left: false }
+  );
