757b158490041d96199a4b97d2762f088e6f4545
Simon Kindström <simon.kindstrom@gmail.com>
2018-05-19 14:15:03 +0200

    User creation and login for front end

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { PureComponent, Fragment } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Loadable from "react-loadable";
@@ -8,30 +8,62 @@ import BottomBar from "./components/BottomBar";
 import Loader from "./components/Loader";
 import EventPage from "./components/EventPage";
 
-const asyncHome = Loadable({
+const AsyncHome = Loadable({
   loader: () => import("./containers/Home"),
   loading: Loader,
   delay: 600
 });
 
-const asyncFeed = Loadable({
+const AsyncFeed = Loadable({
   loader: () => import("./containers/Feed"),
   loading: Loader,
   delay: 600
 });
 
-export const Routes = () => (
-  <MuiThemeProvider>
-    <BrowserRouter>
-      <Fragment>
-        <TopBar />
-        <Route path="/EventPage/:id" exact component={EventPage} />
-        <Route path="/" exact component={asyncHome} />
-        <Route path="/" exact component={asyncFeed} />
-        <BottomBar />
-      </Fragment>
-    </BrowserRouter>
-  </MuiThemeProvider>
-);
+const AsyncSignup = Loadable({
+  loader: () => import("./containers/Signup"),
+  loading: Loader,
+  delay: 600
+});
+
+const AsyncLogin = Loadable({
+  loader: () => import("./containers/Login"),
+  loading: Loader,
+  delay: 600
+});
+
+export class Routes extends PureComponent {
+  state = {
+    user: null
+  };
+
+  setUser = user => this.setState({ user });
+
+  renderWithUser = (Component) => {
+    return props => <Component {...props} user={this.state.user} />;
+  };
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <BrowserRouter>
+          <Fragment>
+            <TopBar />
+            <Route path="/EventPage/:id" exact render={this.renderWithUser(EventPage)} />
+            <Route path="/" exact render={this.renderWithUser(AsyncHome)} />
+            <Route path="/" exact render={this.renderWithUser(AsyncFeed)} />
+            <Route path="/signup" exact component={AsyncSignup} />
+            <Route
+              path="/login"
+              exact
+              render={props => <AsyncLogin {...props} setUser={this.setUser} />}
+            />
+            <BottomBar />
+          </Fragment>
+        </BrowserRouter>
+      </MuiThemeProvider>
+    );
+  }
+}
 
 export default Routes;
