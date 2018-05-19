e3d7245606e5a437408c0521f461b0f9ab615168
icyJoseph <sephxd1234@gmail.com>
2018-05-19 13:45:56 +0200

    feat: responsive landing page

--- client/src/containers/Feed.js
+++ client/src/containers/Feed.js
@@ -3,75 +3,49 @@ import { List } from "material-ui/List";
 import Divider from "material-ui/Divider";
 import Subheader from "material-ui/Subheader";
 import Media from "react-media";
-import { GridList } from "material-ui/GridList";
 
 import EventCard from "../components/EventCard";
 import DesktopCard from "../components/DesktopCard";
-import { events, info } from "../constants";
+import { events, info, today } from "../constants";
 
-const styles = {
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around"
-  },
-  gridList: {
-    width: 600,
-    overflowY: "auto"
-  }
-};
+export const Feed = ({ history }) => {
+  const navigateTo = id => () => {
+    return history.push(`/EventPage/${id}`);
+  };
 
-export const Feed = () => {
   const listOfEvents = events(info);
   const [lastEvent] = listOfEvents.slice(listOfEvents.length - 1);
   const firstEvents = listOfEvents.slice(0, listOfEvents.length - 1);
+
   return (
-    <div>
+    <div
+      style={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        padding: "1%",
+        marginBottom: "100px"
+      }}
+    >
       <Media
         query="(max-width: 1023px)"
         render={() => (
-          <div
-            style={{
-              flex: 1,
-              display: "flex",
-              flexDirection: "column",
-              padding: "1%",
-              marginBottom: "100px"
-            }}
-          >
-            <List>
-              <Subheader>Today</Subheader>
-              {firstEvents.map(e => (
-                <Fragment key={e.id}>
-                  <EventCard {...e} /> <Divider />
-                </Fragment>
-              ))}
-              <EventCard {...lastEvent} />
-            </List>
-          </div>
+          <List>
+            <Subheader>{today}</Subheader>
+            {firstEvents.map(e => (
+              <Fragment key={e.id}>
+                <EventCard {...e} navigateTo={navigateTo(e.id)} />
+                <Divider />
+              </Fragment>
+            ))}
+            <EventCard {...lastEvent} />
+          </List>
         )}
       />
       <Media
         query="(min-width: 1024px)"
         render={() => (
-          <div
-            style={{
-              flex: 1,
-              display: "flex",
-              flexDirection: "column",
-              padding: "1%",
-              marginBottom: "100px"
-            }}
-          >
-            <div style={styles.root}>
-              <GridList cellHeight={120} style={styles.gridList}>
-                <Subheader>Today</Subheader>
-                {listOfEvents.map(event => (
-                  <DesktopCard key={event.id} {...event} />
-                ))}
-              </GridList>
-            </div>
-          </div>
+          <DesktopCard events={listOfEvents} navigateTo={navigateTo} />
         )}
       />
     </div>
